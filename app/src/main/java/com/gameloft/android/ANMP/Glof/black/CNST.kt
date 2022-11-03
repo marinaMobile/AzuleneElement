3a73e50fbe64e71efd8aa77af6c71fb1add2a4e0
Marina <marina@Marinas-MacBook-Pro.local>
2022-11-03 13:03:07 +0300

    first commit

---
+++ app/src/main/java/com/gameloft/android/ANMP/Glof/black/CNST.kt
@@ -0,0 +1,17 @@
+package com.gameloft.android.ANMP.Glof.black
+
+object CNST {
+
+    const val AF_DEV_KEY = "xnR6yTg8MAahihkikTjA6G"
+    const val jsoupCheck = "6a14"
+    const val ONESIGNAL_APP_ID = "7d602098-3ada-41dc-808d-bf631475de36"
+
+    var lru = "http://azuleneelement.xyz/go.php?to=1&"
+
+
+    var odone = "sub_id_1="
+
+    var MAIN_ID: String? = ""
+    var C1: String? = "c11"
+    var D1: String? = "d11"
+}
